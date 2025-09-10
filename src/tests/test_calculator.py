7ae500a6fbf2101f113d2f2cc893ae7deda218b4
Jason Yin <jasonyin2004@gmail.com>
2025-09-10 18:27:50 -0400

    Test pour calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -4,10 +4,22 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 
-from calculator import Calculator
+from src.calculator import Calculator
+
+my_calculator = Calculator()
 
 def test_app():
-    my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "Calculatrice"
+    assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
 
-# TODO: ajoutez les tests
+def test_addition():
+    assert my_calculator.addition(2, 3) == 5
+
+def test_subtraction():
+    assert my_calculator.subtraction(5, 3) == 2
+
+def test_multiplication():
+    assert my_calculator.multiplication(2, 3) == 6
+
+def test_division():
+    assert my_calculator.division(6, 3) == 2
+    assert my_calculator.division(5, 0) == "Erreur : division par zéro"
